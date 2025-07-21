9c52a909b276783d942ac3b672f7c61d51250e66
Lee jeongchan <sjk338285@gmail.com>
2025-07-21 12:56:30 +0900

    chore : 스웨거 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const config = new DocumentBuilder()
+  .setTitle('CodeTogether API')
+  .setDescription('API documentation')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
