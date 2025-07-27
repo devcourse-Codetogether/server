4a3f4881d0bc4368b9bb5aa93b73dc970c1ca12f
Lee jeongchan <sjk338285@gmail.com>
2025-07-27 14:10:44 +0900

    feat : user 로직 구현 ( 미완성 )

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { User } from '../auth/decorators/user.decorator';
+import { UserPayload } from '../auth/types/user-payload.interface';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me/sessions')
+  async getMySessions(@User() user: UserPayload) {
+    return this.userService.getMySessions(user.id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async getMySessions(userId: number) {
+    return this.userRepository.findUserSessions(userId);
+  }
+}
