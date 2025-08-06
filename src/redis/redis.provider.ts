7d4a85d019cd3a568d4a7a4cec93d0beaae87845
Lee jeongchan <sjk338285@gmail.com>
2025-08-06 15:28:55 +0900

    Update redis.provider.ts

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -6,7 +6,7 @@ export const redisProvider = [
     provide: 'REDIS_PUB_CLIENT',
     useFactory: async () => {
       const client = createClient({
-        url: `redis://@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+        url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
       });
       await client.connect();
       return client;
@@ -16,7 +16,7 @@ export const redisProvider = [
     provide: 'REDIS_SUB_CLIENT',
     useFactory: async () => {
       const client = createClient({
-        url: `redis://@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+        url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
       });
       await client.connect();
       return client;
